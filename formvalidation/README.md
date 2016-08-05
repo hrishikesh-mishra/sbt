d4bc617cc98b68f304200bc5947fad78944e8825
Hrishikesh Mishra <sprt.email@gmail.com>
2016-08-06 00:04:45 +0530

    form validation

---
+++ formvalidation/README.md
@@ -0,0 +1,6 @@
+# Spring Boot Examples
+
+Run
+
+mvn clean install && java -jar target/com.hrishikeshmishra-1.0-SNAPSHOT.jar
+
---
+++ formvalidation/src/main/java/com/hrishikeshmishra/formvalidation/Application.java
@@ -0,0 +1,16 @@
+package com.hrishikeshmishra.formvalidation;
+
+/**
+ * Created by hrishikesh.mishra on 05/08/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
