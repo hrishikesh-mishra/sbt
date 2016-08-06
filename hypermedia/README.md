4ae086602066b0aa1f30f0974fb76ef7a5afab69
Hrishikesh Mishra <sprt.email@gmail.com>
2016-08-06 17:47:09 +0530

    hypermedia

---
+++ hypermedia/README.md
@@ -0,0 +1,12 @@
+# Upload File
+
+
+Build Command 
+
+mvn clean install && java -jar target/com.hrishikeshmishra.sbt-1.0-SNAPSHOT.jar
+
+mvn spring-boot:run
+
+Url
+
+http://localhost:8080/greeting
---
+++ hypermedia/src/main/java/com/hrishikeshmishra/hypermedia/Greeting.java
@@ -0,0 +1,22 @@
+package com.hrishikeshmishra.hypermedia;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * Created by hrishikesh.mishra on 06/08/16.
+ */
+public class Greeting extends ResourceSupport {
+
+    private final String content;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
